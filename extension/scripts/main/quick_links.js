240550b5e3d25a36cfba7ec0798d9776e26e6d61
Ning Fu <ning.fu@gmail.com>
2025-06-27 21:59:32 +0800

    Adjust button styles

--- extension/scripts/main/quick_links.js
+++ extension/scripts/main/quick_links.js
@@ -1,18 +1,16 @@
-
-
 function buildTopSitesList(mostVisitedURLs) {
-	var popupDiv = document.getElementById('top-sites-div');	
-	var ul = popupDiv.appendChild(document.createElement('ul'));	
+	var popupDiv = document.getElementById('top-sites-div');
+	var ul = popupDiv.appendChild(document.createElement('ul'));
 
-	for (var i = 0; i < mostVisitedURLs.length; i++) {	
-		var li = ul.appendChild(document.createElement('li'));	
-		var a = li.appendChild(document.createElement('a'));	
-		a.href = mostVisitedURLs[i].url;	
-		a.title = mostVisitedURLs[i].url;	
-		a.appendChild(document.createTextNode(mostVisitedURLs[i].title));	
+	for (var i = 0; i < mostVisitedURLs.length; i++) {
+		var li = ul.appendChild(document.createElement('li'));
+		var a = li.appendChild(document.createElement('a'));
+		a.href = mostVisitedURLs[i].url;
+		a.title = mostVisitedURLs[i].url;
+		a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
 	}
-	popupDiv.appendChild(document.createElement('hr'));	
-}	
+	popupDiv.appendChild(document.createElement('hr'));
+}
 
 // read top sites and show it in page
 function showTopSites() {
@@ -24,7 +22,7 @@ function initTopSites() {
 	// check switch
 	if (readConf('show_top_sites') == 'yes') {
 		// check permission
-		chrome.permissions.contains({permissions:['topSites']}, function(result) {
+		chrome.permissions.contains({ permissions: ['topSites'] }, function (result) {
 			if (result) {
 				// if have permission
 				showTopSites();
@@ -33,57 +31,57 @@ function initTopSites() {
 				writeConf('show_top_sites', 'no');
 			}
 		});
-	}	
-	
+	}
+
 }
 
 // load custom bookmarks
 function initCustomBookmarks() {
 	var bookmarks = readConf('custom_bkmk_list');
 	if (bookmarks != null && bookmarks.length > 0) {
-		var popupDiv = document.getElementById('top-sites-div');	
-		var ul = popupDiv.appendChild(document.createElement('ul'));	
-		for (var i = 0; i < bookmarks.length; i++) {	
-			var li = ul.appendChild(document.createElement('li'));	
-			var a = li.appendChild(document.createElement('a'));	
+		var popupDiv = document.getElementById('top-sites-div');
+		var ul = popupDiv.appendChild(document.createElement('ul'));
+		for (var i = 0; i < bookmarks.length; i++) {
+			var li = ul.appendChild(document.createElement('li'));
+			var a = li.appendChild(document.createElement('a'));
 
 			// Add SVG icon at the beginning
 			var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 			svg.setAttribute("width", "16");
 			svg.setAttribute("height", "16");
-			svg.setAttribute("viewBox", "0 0 3.04 3.04");
-			svg.setAttribute("class", "icon");
-			svg.innerHTML = '<path fill="#ffffff" d="M16.417 9.5a7.919 7.919 0 0 1-15.212 3.082A7.87 7.87 0 0 1 .584 9.5a.554.554 0 0 1 1.109 0 6.81 6.81 0 0 0 13.081 2.65 6.811 6.811 0 0 0-9.66-8.557 6.859 6.859 0 0 0-1.847 1.554h1.276a.554.554 0 0 1 0 1.108h-2.61a.554.554 0 0 1-.555-.554V3.09a.554.554 0 0 1 1.109 0v1.262A7.898 7.898 0 0 1 8.5 1.583 7.911 7.911 0 0 1 16.417 9.5zm-5.181 3a.554.554 0 0 1-.784 0l-2.42-2.42a.552.552 0 0 1-.22-.441V5.168a.554.554 0 1 1 1.11 0v4.234l2.314 2.315a.554.554 0 0 1 0 .784z"/>';
+			svg.setAttribute("viewBox", "0 0 0.32 0.32");
+			svg.setAttribute("style", "margin-right: 6px; vertical-align: -2px;");
+			svg.innerHTML = '<path fill="#fff" d="M0.214 0.009a0.16 0.16 0 1 1 -0.199 0.219 0.02 0.02 0 1 1 0.036 -0.017 0.12 0.12 0 1 0 0.028 -0.14l0.015 0.015A0.02 0.02 0 0 1 0.08 0.12H0V0.04A0.02 0.02 0 0 1 0.034 0.026l0.017 0.017a0.16 0.16 0 0 1 0.163 -0.033M0.16 0.08a0.02 0.02 0 0 1 0.02 0.02v0.052l0.034 0.034a0.02 0.02 0 0 1 -0.028 0.028L0.14 0.168V0.1A0.02 0.02 0 0 1 0.16 0.08"/>';
 			a.appendChild(svg);
 
 			// Handle custom actions
 			if (bookmarks[i].action) {
 				a.href = '#';
-				a.addEventListener('click', (function(action) {
-					return function(e) {
+				a.addEventListener('click', (function (action) {
+					return function (e) {
 						e.preventDefault();
 						handleCustomAction(action);
 					};
 				})(bookmarks[i].action));
 				a.title = 'Custom Action: ' + bookmarks[i].name;
 			} else if (bookmarks[i].url) {
-				a.href = bookmarks[i].url;	
+				a.href = bookmarks[i].url;
 				a.title = bookmarks[i].url;
 			} else {
 				// Fallback for invalid bookmark entries
 				a.href = '#';
 				a.title = 'Invalid bookmark';
 			}
-			
-			a.appendChild(document.createTextNode(bookmarks[i].name));	
-		}	
-		popupDiv.appendChild(document.createElement('hr'));	
+
+			a.appendChild(document.createTextNode(bookmarks[i].name));
+		}
+		popupDiv.appendChild(document.createElement('hr'));
 	}
 }
 
 // Handle custom actions for bookmarks
 function handleCustomAction(action) {
-	switch(action) {
+	switch (action) {
 		case 'random_bing_wallpaper':
 			var today = new Date();
 			var yearMin = 2010;
